fe210412adcb40912fa62ecaba1d7c6c846acc33
Lyle Scott <Lyle.C.Scott@icloud.com>
2017-10-12 20:37:56 -0700

    add api key

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -15,7 +15,8 @@ angular.module('myApp.view1', ['ngRoute'])
 
             $http({
                 method: 'GET',
-                url: 'https://data.seattle.gov/api/views/cf52-s8er/rows.json'
+                url: 'https://data.seattle.gov/api/views/cf52-s8er/rows.json?api_key=SCC1c0Cove7ypmBeuf3dTX2WZOk6\n' +
+                'qEfCAki6MoNi'
             }).then(function successCallback(response) {
                 console.log(response.data.data);
             }, function errorCallback(response) {
