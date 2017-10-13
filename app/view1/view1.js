1580d434628585b8a65212dbf06c1f56be7a2ded
Lyle Scott <Lyle.C.Scott@icloud.com>
2017-10-12 20:02:02 -0700

    still setting up

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -2,15 +2,24 @@
 
 angular.module('myApp.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view1', {
+            templateUrl: 'view1/view1.html',
+            controller: 'View1Ctrl'
+        });
+    }])
 
-.controller('View1Ctrl', [function() {
+    .controller('View1Ctrl', [
+        '$http',
+        function ($http) {
 
-  console.log("moosh baby");
+            $http({
+                method: 'GET',
+                url: 'https://data.seattle.gov/api/views/cf52-s8er/rows.json'
+            }).then(function successCallback(response) {
+                console.log(response.data.data);
+            }, function errorCallback(response) {
+                console.log(response);
+            });
 
-}]);
+        }]);
